918fd6d85be0ec6d1c56abc5b941ce6aea12f483
mbdavid2 <mbdavid297@gmail.com>
2018-04-04 20:18:33 +0200

    mmmm...

---
+++ Practica-1/Ex2-3 Algoritmes ExpRecurrents/tests.m
@@ -0,0 +1,27 @@
+%% 2) Pi
+% for k = 1:12
+%    n = 7^k;
+%    pi_alg(n);
+% end
+
+%% Pi efficient
+% El for està dins de la funció, aquí li diem la k màxima
+%    k = 5;
+%    effPi(k);
+
+%% Golden Ratio
+format long
+% phiCorrect = (1 + sqrt(5))/2;
+% 
+% %% Using Fibonacci
+% for n = 5:1000
+%    Orfib(n);
+% end
+
+% % %% Using continued fraction
+for n = 5:1000
+   Orfract(n);
+end
+
+phiCorrect = (1 + sqrt(5))/2
+
